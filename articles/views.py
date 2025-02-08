84bff9e62e2da1b7582b5e76e3a0c9c5cf2f0369
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-08 09:57:03 +0900

    new function named demo in articles/views.py

---
+++ articles/views.py
@@ -0,0 +1,49 @@
+from .models import Article
+from .forms import ArticleForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+def home_view(request):
+    articles = Article.objects.all()
+    return render(request, 'home.html', {'articles': articles})
+
+
+@login_required
+def article_create_view(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('article-detail', id=article.id)
+    else:
+        form = ArticleForm()
+    return render(request, 'article/article_log.html', {'form': form})
+
+
+
+def article_detail_view(request, id):
+    article = Article.objects.get(id=id)
+    return render(request, 'articles/article_detail.html', {'article': article})
+
+def demo(request):
+    articles = Article.objects.all()
+
+
+
+# def article_create_viewold1(request):
+#     form = ArticleForm()
+#     context = {
+#         'form': form,
+#     }
+    
+#     if request.method == "POST":
+#         form = ArticleForm(request.POST)
+#         if form.is_valid():
+#             title = form.cleaned_data.get('title')
+#             content = form.cleaned_data.get('content')
+        
+#             article_object = Article.objects.create(title=title, content=content)
+#             context['object'] = article_object
+#             context['created'] = True
+#             return redirect('home')
+#     return render(request, 'articles/article_create.html', context=context)
