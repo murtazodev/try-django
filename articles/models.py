c32947f03fe98b45d61d550fd8a095ec2331e921
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-10 17:23:25 +0900

    slug field to model

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils.timezone import localtime
+from django.utils.text import slugify
 
 class Article(models.Model):
     title = models.CharField(max_length=120)
+    slug = models.SlugField(blank=True, null=True)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
@@ -15,3 +17,7 @@ class Article(models.Model):
     
     def __str__(self):
         return f"{self.id} - {self.title}"
+    
+    def save(self, *args, **kwargs):    
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
