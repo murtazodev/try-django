e117f1e7f38b8130ae3955390c70a3b46975a52d
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-11 12:48:22 +0900

    change accordingly based on slug

--- articles/urls.py
+++ articles/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # path('index/', views.index_view, name='index'),
     path('article/create/', views.article_create_view, name='article-create'),
     path('article/<slug:slug>/', views.article_detail_view, name='article-detail'),
-    path('article/<int:id>/update/', views.article_update_view, name='article-update'),
+    path('article/<slug:slug>/update/', views.article_update_view, name='article-update'),
     # path('articles', views.articles, name='article-list'),
 ]
