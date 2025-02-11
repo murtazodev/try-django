f448dd5e65d3d09da18112723d63ff849924c7b9
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-11 12:49:16 +0900

    add tests to slugify, slug field of Article model

--- articles/tests.py
+++ articles/tests.py
@@ -1,4 +1,20 @@
 from django.test import TestCase
-
+from django.utils.text import slugify
+from .models import Article
 # Create your tests here.
+class ArticleTestCase(TestCase):
+    def setUp(self):
+        self.article1 = Article.objects.create(title='Test Article 1')
+        self.article2 = Article.objects.create(title='Test Article 2')
+        self.article3 = Article.objects.create(title='Test Article 3')
+
+    def test_article_uniqueness(self):
+        self.assertNotEqual(self.article1.slug, self.article2.slug)
+        self.assertNotEqual(self.article1.slug, self.article3.slug)
+
+    def test_slug_automatic_generation(self):
+        article = Article.objects.create(title='New Article Without Slug')
+        expected_slug= slugify(article.title)
+        
+        self.assertTrue(article.slug.startswith(expected_slug), True)
 
