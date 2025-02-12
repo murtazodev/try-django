a8a3a65922025c00ca476bb54137107384115145
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-13 07:23:32 +0900

    register model to recipes->admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import*
 
-admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user']
+    readonly_fields = ['user', 'created_at', 'updated_at']
+
+admin.site.register(Recipe, RecipeAdmin)
+
+    
