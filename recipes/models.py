8ce9448e4ea8a7cb681763c953e9cdb388607440
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-12 14:59:50 +0900

    add Recipe and RecipeIngredient classes to article/models.py

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Recipe(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    directions = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=220)
+    description = models.TextField(blank=True, null=True)
+    quantity = models.CharField(max_length=50)
+    unit = models.CharField(max_length=50)
+    directions = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
