0d0592eb53d07ddcd3258e2d240fbdbdae8787e0
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-13 07:51:02 +0900

    recipes admin conf

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,9 +3,14 @@ from .models import*
 
 admin.site.register(RecipeIngredient)
 
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ['name', 'user']
-    readonly_fields = ['user', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+    raw_id_fields = ['user']
 
 admin.site.register(Recipe, RecipeAdmin)
 
