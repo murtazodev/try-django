7148a399b8f12f66d01d0cf38132b7fab4cf5944
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-13 10:44:00 +0900

    add recipte tests

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import*
 
-# Create your tests here.
+
+User = get_user_model()
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.user_a = User.objects.create_user(username='mrt', password='5678m')
+    
+    def test_user_pswd(self):
+        self.assertTrue(self.user_a.check_password('5678m'))
+
+class RecipeTestCase(TestCase):
+    def setUp(self):
+        self.user_a = User.objects.create_user(username='mrt', password='5678m')
+        self.recipe_a = Recipe.objects.create(name='recipe_a', user=self.user_a)
+        self.recipe_b = Recipe.objects.create(name='recipe_b', user=self.user_a)
+
+    def test_user_recipe_reverse_count(self):
+        qs = self.user_a.recipe_set.all()
+        print(qs.first().name)  
+        self.assertEqual(qs.count(), self.user_a.recipe_set.count())
+
+    def test_user_recipe_forward_count(self):
+        user = self.user_a 
+        qs = Recipe.objects.filter(user=user)
+        print(qs.first().name)  
+        self.assertEqual(qs.count(), self.user_a.recipe_set.count())
+
+
