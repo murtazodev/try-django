6a2caf364cb4c799e828962dfa59296e55a33345
toshniyozov <toshniyozovmurtazo.r538@gmail.com>
2025-02-14 11:33:51 +0900

    quantity as float validation, migration, quantity_float as a readonly field etc

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import*
+from .models import Recipe, RecipeIngredient
 
 admin.site.register(RecipeIngredient)
 
 class RecipeIngredientInline(admin.StackedInline):
     model = RecipeIngredient
     extra = 0
+    readonly_fields = ['quantity_float']
 
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ['name', 'user']
--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from .validators import validate_unit_of_measurement
+from .utils import number_str_to_float
+
 
 class Recipe(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
@@ -16,9 +18,19 @@ class RecipeIngredient(models.Model):
     name = models.CharField(max_length=220)
     description = models.TextField(blank=True, null=True)
     quantity = models.CharField(max_length=50)
+    quantity_float = models.FloatField(blank=True, null=True)
     unit = models.CharField(max_length=50, validators=[validate_unit_of_measurement])
     directions = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        qty_float, success = number_str_to_float(self.quantity)
+        if success:
+            self.quantity_float = qty_float
+        else:
+            self.quantity_float = None
+        super().save(*args, **kwargs)
+
 
